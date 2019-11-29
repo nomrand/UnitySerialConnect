9eb882102207319dfa9a3a9208c712d38e1221fb
nomrand <42882840+nomrand@users.noreply.github.com>
2019-11-29 17:41:09 +0700

    ver 1.0

--- Assets/UnitySerialConnect/Scripts/SerialConnectHandler.cs
+++ Assets/UnitySerialConnect/Scripts/SerialConnectHandler.cs
@@ -1,64 +1,131 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/// Copyright [2019] [https://github.com/nomrand]
+/// 
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+/// 
+///     http://www.apache.org/licenses/LICENSE-2.0
+/// 
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
 using System.IO.Ports;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.Events;
 
+/// <summary>
+/// Control serial connection in Unity
+/// This script is designed to be used by attaching into a Game Object in the Scene.
+/// (If the Scene has a Game Object which is attached by this script,
+/// it will automatically open/close the Serial Port)
+/// Before used, must set the properties in the inspector in Unity.
+/// </summary>
+/// <example>
+/// For Sending (Just call the static method like below)
+/// <code>
+/// SerialConnectHandler.Write(message);
+/// </code>
+///
+/// For Recieving
+/// 1. Make a method with a string parameter like below
+/// <code>
+/// public void recieved(string message)
+/// {
+/// 	// Do something ...
+/// }
+/// </code>
+/// 2. Then, set the "Recieved Event" property of the Game Object
+/// which is attached by this script
+/// (set as a "Dinamic" method, not "Static Parameter" method)
+/// </example>
 public class SerialConnectHandler : MonoBehaviour
 {
+
+    /// <summary>
+    /// Event handler which can be called when Serial Connection Data received
+    /// (It can be set on inspector window in Unity)
+    /// </summary>
+    [SerializeField]
+    public RecievedEvent receivedEvent;
     [System.Serializable]
     public class RecievedEvent : UnityEvent<string> { }
 
-    [SerializeField]
-    public RecievedEvent receivedEvent;
 
-
+    /// <summary>
+    /// Port name of Serial Connection<br>
+    /// Windows : Such as COM1/COM2/... (It can be checked by "Device Manager"<br>
+    /// Mac : Such as /dev/tty.usbmodem1421<br>
+    /// Linux : Such as /dev/ttyUSB0<br>
+    /// </summary>
     public string portName = "COM1";
+
+    /// <summary>
+    /// BaudRate of Serial Connection (It must be same with connection destination)
+    /// </summary>
     public int baudRate = 9600;
 
-    private SerialPort serialPort_;
 
-    static private SerialPort static_serialPort_;
+    /// <summary>
+    /// Opened Serial Port (It is static for convenience of sending message)
+    /// </summary>
+    static private SerialPort serialPort_;
+
+    /// <summary>
+    /// Recieved Message (When not recieved yet, it must be null)
+    /// </summary>
+    private string message_;
 
     private Thread thread_;
     private bool isRunning_ = false;
 
-    private string message_;
-    private bool isNewMessageReceived_ = false;
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// Initialize (open port, etc)
+    /// </summary>
     void Start()
     {
-        serialPort_ = new SerialPort(portName, baudRate);
-        serialPort_.Open();
+        SerialPort serialPort = new SerialPort(portName, baudRate);
+        serialPort.Open();
 
-        static_serialPort_ = serialPort_;
+        serialPort_ = serialPort;
         isRunning_ = true;
 
+        // start "Message Read Thread"
         thread_ = new Thread(Read);
         thread_.Start();
 
         Debug.Log("Open Serial Port(" + portName + ")  baudRate=" + baudRate);
     }
 
+    /// <summary>
+    /// Call registered handler method
+    /// ("Invoke" can not be called in "Message Read Thread", but main-thread can)
+    /// </summary>
     void Update()
     {
-        if (isNewMessageReceived_)
+        if (message_ != null)
         {
+            // message_ is only set when data recieved
             receivedEvent.Invoke(message_);
         }
-        isNewMessageReceived_ = false;
+        message_ = null;
     }
 
+    /// <summary>
+    /// Finalize (close port, etc)<br>
+    /// This method will be called when "Stop" (in Unity Editor), or stop the application
+    /// </summary>
     void OnApplicationQuit()
     {
         isRunning_ = false;
-        static_serialPort_ = null;
-        Debug.Log("Close Serial Port(" + portName + ")");
 
         if (thread_ != null && thread_.IsAlive)
         {
+            // if connection is blocking, and not return
+            // operation would stop here...
             thread_.Join();
         }
 
@@ -67,8 +134,13 @@ public class SerialConnectHandler : MonoBehaviour
             serialPort_.Close();
             serialPort_.Dispose();
         }
+        serialPort_ = null;
+        Debug.Log("Close Serial Port(" + portName + ")");
     }
 
+    /// <summary>
+    /// Read from Serial Connection (by "Message Read Thread")
+    /// </summary>
     private void Read()
     {
         while (isRunning_ && serialPort_ != null && serialPort_.IsOpen)
@@ -76,8 +148,12 @@ public class SerialConnectHandler : MonoBehaviour
             try
             {
                 string line = serialPort_.ReadLine();
+
+                // In this "Message Read Thread",
+                // we can not control Game Objects in Scene.
+                // So, recieved message should be set temporary.
+                // And pass to the handler methods indirectly.
                 message_ = line;
-                isNewMessageReceived_ = true;
             }
             catch (System.Exception e)
             {
@@ -86,11 +162,16 @@ public class SerialConnectHandler : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Write to Serial Connection (with Line Feed)
+    /// </summary>
     static public void Write(string message)
     {
         try
         {
-            static_serialPort_.Write(message);
+            // In connection destination,
+            // Line Feed (end of the line) will be used to understand one chunk of data
+            serialPort_.WriteLine(message);
         }
         catch (System.Exception e)
         {
--- Assets/UnitySerialConnect/Scripts/UseExample.cs
+++ Assets/UnitySerialConnect/Scripts/UseExample.cs
@@ -1,28 +1,54 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// This is a example of using "SerialConnectHandler"
+/// </summary>
 public class UseExample : MonoBehaviour
 {
     public Text recievedText;
 
+    /// <summary>
+    /// Data Recieve
+    /// This method must be set in "Recieved Event" in "SerialConnectHandler"
+    /// (as a Dinamic Parameter Method)
+    /// </summary>
+    /// <param name="message">Recieved Data</param>
     public void recieved(string message)
     {
         int lightSensor = 0;
         double tempSensor = 0.0;
-        string[] messages = message.Split(',');
 
+        // In this example,
+        // Recieved Message must be "xxxx,yyyy"
+        // xxxx is a int value of Light Sensor
+        // yyyy is a double value of Temperature Sensor
+
+        // To use "SerialConnectHandler" correctly,
+        // you must check Recieved Data.
+        // Because, Serial Port can be used as many purposes,
+        // So, this data may not be a true data which you want.
+        string[] messages = message.Split(',');
         if (messages.Length < 2)
         {
+            // True data has a ',' 
+            // and at least 2 parts of values (Light & Temperature)
+            // If not correct, it is not a true data
             return;
         }
+        // recieved data is a string value,
+        // So, you may convert string to numbers
         lightSensor = int.Parse(messages[0]);
         tempSensor = double.Parse(messages[1]);
 
         recievedText.text = "light:" + lightSensor + "\n" +
                             "temp:" + tempSensor;
-
     }
 
+    /// <summary>
+    /// Data Send
+    /// </summary>
+    /// <param name="message">Send Data</param>
     public void send(string message)
     {
         Debug.Log("SEND:" + message);
