7734fb4d182baa248354c59faa56b81e69f65212
nomrand <42882840+nomrand@users.noreply.github.com>
2019-11-29 14:37:07 +0700

    init

---
+++ Assets/UnitySerialConnect/Scripts/SerialConnectHandler.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SerialConnectHandler : MonoBehaviour
+{
+    [System.Serializable]
+    public class RecievedEvent : UnityEvent<string> { }
+
+    [SerializeField]
+    public RecievedEvent receivedEvent;
+
+
+    public string portName = "COM1";
+    public int baudRate = 9600;
+
+    private SerialPort serialPort_;
+
+    static private SerialPort static_serialPort_;
+
+    private Thread thread_;
+    private bool isRunning_ = false;
+
+    private string message_;
+    private bool isNewMessageReceived_ = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        serialPort_ = new SerialPort(portName, baudRate);
+        serialPort_.Open();
+
+        static_serialPort_ = serialPort_;
+        isRunning_ = true;
+
+        thread_ = new Thread(Read);
+        thread_.Start();
+
+        Debug.Log("Open Serial Port(" + portName + ")  baudRate=" + baudRate);
+    }
+
+    void Update()
+    {
+        if (isNewMessageReceived_)
+        {
+            receivedEvent.Invoke(message_);
+        }
+        isNewMessageReceived_ = false;
+    }
+
+    void OnApplicationQuit()
+    {
+        isRunning_ = false;
+        static_serialPort_ = null;
+        Debug.Log("Close Serial Port(" + portName + ")");
+
+        if (thread_ != null && thread_.IsAlive)
+        {
+            thread_.Join();
+        }
+
+        if (serialPort_ != null && serialPort_.IsOpen)
+        {
+            serialPort_.Close();
+            serialPort_.Dispose();
+        }
+    }
+
+    private void Read()
+    {
+        while (isRunning_ && serialPort_ != null && serialPort_.IsOpen)
+        {
+            try
+            {
+                string line = serialPort_.ReadLine();
+                message_ = line;
+                isNewMessageReceived_ = true;
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogWarning(e.Message);
+            }
+        }
+    }
+
+    static public void Write(string message)
+    {
+        try
+        {
+            static_serialPort_.Write(message);
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);
+        }
+    }
+}
---
+++ Assets/UnitySerialConnect/Scripts/UseExample.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UseExample : MonoBehaviour
+{
+    public Text recievedText;
+
+    public void recieved(string message)
+    {
+        int lightSensor = 0;
+        double tempSensor = 0.0;
+        string[] messages = message.Split(',');
+
+        if (messages.Length < 2)
+        {
+            return;
+        }
+        lightSensor = int.Parse(messages[0]);
+        tempSensor = double.Parse(messages[1]);
+
+        recievedText.text = "light:" + lightSensor + "\n" +
+                            "temp:" + tempSensor;
+
+    }
+
+    public void send(string message)
+    {
+        Debug.Log("SEND:" + message);
+        SerialConnectHandler.Write(message);
+    }
+}
